d4a351f7b0c279c3c9eae71acd1c68ebed925805
Belka9823 <168011744+Belka9823@users.noreply.github.com>
2026-01-14 12:12:17 +0300

    Add files via upload

---
+++ Js_1/JS.js
@@ -0,0 +1,12 @@
+console.log('He110, w0r1d!');
+a = 5;
+b = 4;
+c = 1;
+d = 'hello';
+e = '10';
+sum = a + b + c + d + e + e;
+console.log(a + b + c + d + e + e); 
+console.log(sum); 
+typeof (sum);
+console.log (typeof(sum));
+
